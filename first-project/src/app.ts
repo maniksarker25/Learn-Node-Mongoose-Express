029d8f7a5b17c94b065e756862ea126359e6a8c8
Manik <tonysins43@gmail.com>
2023-11-17 00:25:28 +0600

    s

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -1,9 +1,14 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
-const port = 3000;
+
+// parser
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  const a = 10;
+  res.send(a);
 });
 
 export default app;
--- first-project/src/server.ts
+++ first-project/src/server.ts
@@ -12,3 +12,5 @@ async function main() {
     console.log(error);
   }
 }
+
+main();
