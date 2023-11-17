ad55313aa31f495c9c2d647a215343d44515865d
Manik <tonysins43@gmail.com>
2023-11-17 09:41:59 +0600

    s

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -1,12 +1,12 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 });
--- first-project/src/app/config/index.ts
+++ first-project/src/app/config/index.ts
@@ -1,6 +1,6 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
